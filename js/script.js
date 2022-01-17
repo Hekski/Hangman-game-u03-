fc114433657b8e9aa8f8d4b4f917fbe9040a1fd6
LesHank <henrik.berglund@gmail.com>
2022-01-17 21:47:23 +0100

    add solution for winning with words containing more than one of the same letter

--- js/script.js
+++ js/script.js
@@ -1,163 +1,156 @@
-// Ursäkta ljudet, skulle väl egentligen haft en ljud på/av knapp men hanns inte med :)
-// Hade även kvar att lösa estetiska grejer, men även bättre reposnsivitet.
+// DOM-nodes
 
-const startGameBtn = document.querySelector('#start'); // DOM-nod: knappen som du startar spelet med
-const letterBtn = document.querySelectorAll('#letterButtons button'); // Array av DOM-noder: Knapparna för bokstäverna
+const startGameBtn = document.querySelector("#start"); // The start button
+const letterBtn = document.querySelectorAll("#letterButtons button"); // The letter buttons
+const word = document.querySelector("#word"); // The container of the placeholders for the word letters
+const letterBoxes = document.querySelector(".letterBoxes"); // The placeholders for the word letters
+const message = document.querySelector("#message"); // Placeholder to recieve messages
+const hangmanImg = document.querySelector("#hangman"); // Path to the image that is replaced in case of a wrong guess
+const gruntImg = document.querySelector("#grunt"); // Decorative picture
 
-const word = document.querySelector('#word');
-const letterBoxes = document.querySelector('.letterBoxes');
-const message = document.querySelector('#message'); // DOM-nod: Ger olika meddelanden
-const hangmanImg = document.querySelector('#hangman'); // Sträng: sökväg till bild som byts ut vid fel gissning
-const gruntImg = document.querySelector('#grunt'); // Sträng: Dekorativ bild
+// Variables
 
+let selectedWord; // One of the words chosen by the random generator
+let guessedLetter; // The user's guessed letter captured by an event listener
+let foundLetter; // Value for the matched guessed letter and the letters in the current word
 
-let selectedWord;       // Sträng: ett av orden valt av en slumpgenerator
-let guessedLetter;      // Användarens gissade bokstav uppfångad av en eventlistener
-let foundLetter;        // Värde för matchad gissad bokstav och bokstäver i det aktuella ordet 
+// Values for the amount of right or wrong guesses
 
-let wrongGuesses = 0;   // Värde för antal felaktiga gissningar
-let rightGuesses = 0;   // Värde för antal rätta gissningar
+let wrongGuesses = 0;
+let rightGuesses = 0;
 
-let letterBoxEls;       // Array av DOM-noder: Rutorna där bokstäverna ska stå
-let liArray;            // Array med koppling till li-objekten
+// Array connecting the Li-objects
 
+let liArray;
 
-// Eventlisteners
+// Eventlistener
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener("click", startGame);
 
+// Array: The words of the game
 
-// Array: med spelets alla ord
+const wordList = ["squid", "masks", "umbrella", "Cookie", "prize", "money"];
 
-const wordList = [
-  'squid', 
-  'Gi-hun', 
-  'masks', 
-  'umbrella', 
-  'Deok-su', 
-  'prize', 
-  'money',
-];
-
-// Ljudfiler
+// Sound files
 
 let sounds = {
-  doll: 'doll.mp3',
-  fly: 'fly.mp3',
-  green: 'green.mp3',
-  red: 'red.mp3',
-  track: 'track.mp3',
+  doll: "doll.mp3",
+  fly: "fly.mp3",
+  green: "green.mp3",
+  red: "red.mp3",
+  track: "track.mp3",
 };
 
 window.onload = startState;
 
 function startState() {
   disableLetterButtons();
+  message.textContent = "Welcome...";
+
+  // document.body.style.animation = 0;
 }
 
-// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som återställer variabler och startar ett nytt spel
+// Function that starts the game at the push of a button, and calls other functions
 
 function startGame() {
   activateLetterButtons();
-  hangmanImg.src = '/images/start.png';
+  hangmanImg.src = "images/start.png";
   startGameBtn.disabled = true;
-  message.textContent = 'Pick your letter...';
+  message.textContent = "Pick your letter...";
   selectedWord = getRandomWord();
   generateLIs();
-  console.log('Game started with word: ' + selectedWord);
+  // console.log("Game started with word: " + selectedWord);
 
-  liArray = document.querySelector('#word').childNodes;
-  document.body.style.backgroundColor = "black";
-  activateLetterButtons();
+  liArray = document.querySelector("#word").childNodes;
   playSound(sounds.doll);
 }
 
-// Funktion som slumpar fram ett ord ut ordlistan
+// Function that randomizes a word out of the word list
 
 function getRandomWord() {
   let randomNo = Math.floor(Math.random() * wordList.length);
   return wordList[randomNo].toUpperCase();
-} 
+}
 
-// Funktion som skapar li-objekt efter antalet bokstäver i utvalt ord
+// Function that creates li objects according to the number of letters in the selected word
 
 function generateLIs() {
   const numberOfLetters = selectedWord.length;
-    word.innerHTML = ''; // Tack Björn :)
-    for (let i = 0; i < numberOfLetters; i++) {
-    let listItem = document.createElement('li');
-    listItem.innerHTML = '_';
-    hangmanImg.src = '/images/h0.png';
+  word.innerHTML = "";
+  for (let i = 0; i < numberOfLetters; i++) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = "_";
+    hangmanImg.src = "images/h0.png";
     word.append(listItem);
   }
 }
 
-// Funktion som körs när du trycker på bokstäverna och gissar bokstav
+// Function that runs when a guessed letter is clicked
 
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
   let element = e.target;
-  if(element.parentNode.parentNode.id == 'letterButtons'){ 
+  if (element.parentNode.parentNode.id == "letterButtons") {
     guessedLetter = element.value;
     element.disabled = true;
-    console.log('Clicked button ' + guessedLetter);
+    // console.log("Clicked button " + guessedLetter);
     gameState();
   }
 });
 
-//Function som sköter spelets förlopp
+// Function that handles the course of the game
 
 function gameState() {
-  console.log('gissad bokstav ' + guessedLetter)
+  // console.log("gissad bokstav " + guessedLetter);
   foundLetter = 0;
-  for(let i = 0; i < selectedWord.length; i++) {
+  for (let i = 0; i < selectedWord.length; i++) {
     let currentLetter = selectedWord.charAt(i);
-      if(guessedLetter == currentLetter) {        
-        liArray[i].innerHTML = currentLetter;        
-        foundLetter++;
-      }
+    if (guessedLetter == currentLetter) {
+      liArray[i].innerHTML = currentLetter;
+      foundLetter++;
     }
-    if (foundLetter == 0) {
-      message.textContent = 'Wrong answer...';
-      wrongGuesses++; //öka pott av missar som inte får överstiga selectedWord.length
-      hangmanImg.src = '/images/h' + wrongGuesses + ".png"; //rita gubbe
-      playSound(sounds.red);
-
-      lose(); // Kolla möjlig förlust
-
-    } else {
-      message.textContent = 'Right answer, continue...';
-      rightGuesses++;
-      playSound(sounds.green);
-      win(); //Kolla möjlig vinst
   }
-  console.log(wrongGuesses + ' antal antal felgissningar');
+  if (foundLetter == 0) {
+    message.textContent = "Wrong answer...";
+    wrongGuesses++; // An increase of the value of missed guesses that can not exceed selectedWord.length
+    hangmanImg.src = "images/h" + wrongGuesses + ".png"; // Adding i picture if the guess is wrong
+    playSound(sounds.red);
+
+    lose(); // Checking for possible loss
+  } else {
+    message.textContent = "Right answer, continue...";
+    rightGuesses = rightGuesses + foundLetter;
+    playSound(sounds.green);
+    win(); // Checking for possible win
+  }
 }
-  
-// Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
+
+// Functions that are called upon win or loss
 
 function win() {
   if (rightGuesses == selectedWord.length) {
-    console.log(rightGuesses + ' antal rightGuesses, VINST!');
-    message.textContent = 'Congratulations...';
-    startGameBtn.textContent = 'Play again?';
+    // console.log(rightGuesses + " antal rightGuesses, VINST!");
+    message.textContent = "Congratulations...";
+    startGameBtn.textContent = "Play again?";
     playSound(sounds.track);
-    hangmanImg.src = '/images/win.png';
+    document.body.style.animation = "fadeBackGroundWin 2s forwards 1";
+    document.body.style.backgroundColor = "black";
+    hangmanImg.src = "images/win.png";
     endState();
   }
 }
 
 function lose() {
   if (wrongGuesses == 7) {
-  message.textContent = 'You lose...';
-  startGameBtn.textContent = 'Play again?';
-  document.body.style.animation = "fadeBackGround 3s forwards 1";
-  playSound(sounds.fly);
-  endState();
+    message.textContent = "You lose...";
+    startGameBtn.textContent = "Play again?";
+    document.body.style.animation = "fadeBackGround 3s forwards 1";
+    document.body.style.backgroundColor = "black";
+    playSound(sounds.fly);
+    endState();
   }
 }
 
-// Funktion för att återställa knapparna
+// Function to reset the buttons
 
 function disableLetterButtons() {
   for (let i = 0; i < letterBtn.length; i++) {
@@ -165,11 +158,15 @@ function disableLetterButtons() {
   }
 }
 
+// Function to activate the buttons
+
 function activateLetterButtons() {
   for (let i = 0; i < letterBtn.length; i++) {
     letterBtn[i].disabled = false;
   }
 }
+
+// Function that handles the end phase of the game
 
 function endState() {
   rightGuesses = 0;
@@ -178,8 +175,10 @@ function endState() {
   startGameBtn.disabled = false;
 }
 
+// Function to play soundfiles
+
 function playSound(file, volume = 0.4) {
-  let audio = new Audio('./sound/' + file);
+  let audio = new Audio("./sound/" + file);
   audio.volume = volume;
   audio.play();
 }
